4fbf2cad580fddb946bc7d1c05a1e3e13c342fa9
Violette Pumkins <74327188+Violette-Pumkins@users.noreply.github.com>
2021-01-04 09:40:34 +0100

    bootstrap recherche et acceuil, js bdd

---
+++ BDD.js
@@ -0,0 +1,15 @@
+// variables nécessaires
+document.getElementById("")
+
+
+
+
+// créer cards
+// attribuer informations
+// retourne une fiche produit
+function createCards{
+ album.forEach(element => {
+     document.createElement
+ });
+
+}
