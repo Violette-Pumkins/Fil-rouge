99f400b2ca0d94220245fa7f8c03fe0c60e53c5e
Violette Pumkins <74327188+Violette-Pumkins@users.noreply.github.com>
2021-01-07 16:27:03 +0100

    mdp et mail a faire +recherche

--- BDD.js
+++ BDD.js
@@ -1,24 +1,38 @@
 // variables nécessaires
 const val = albums.keys();
+var pass = document.getElementById("password");
+var mail = document.getElementById("email");
+// var invalidpass= document.getElementById("invalidpass");
+// var invalidmail= document.getElementById("invalidmail");
+// var decipass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+var decipass = /^[0-9]{4}$/;
+// var decimail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
+// création
+initCard();
+
+// bouton abonnement
+ var ok = document.getElementById("ok");
+ ok.addEventListener("click",checkpass(pass));
 
 
-// initialiser cards
+
+//  intitialiser cards
 function initCard() {
   albums.forEach(e => {
     createCards(e);
-
-
   });
-
 }
-
 // créer cards
 // attribuer informations
 function createCards(bdd) {
-
   var idbd = val.next().value;
   var serie = series.get(bdd.idSerie);
-  var nomart = auteurs.get(bdd.idAuteur)
+  var nomart = auteurs.get(bdd.idAuteur);
+
+  // creation cards bdd acceuil et autres
+
+
 
   // creation div container
   var divCol = document.createElement("div");
@@ -30,32 +44,21 @@ function createCards(bdd) {
   var divPanel = document.createElement("div");
   divPanel.setAttribute("class", "card");
   divCol.appendChild(divPanel);
-
   //  abonnement div card click
-
   divPanel.addEventListener("click", function (e) { localStock(val) })
-
-
   //image
   var newimg = document.createElement("img");
   newimg.setAttribute("class", "card-img-top p-1");
   var nomFic = serie.nom + '-' + bdd.numero + '-' + bdd.titre;
-  newimg.src = "albumsMini/" + nomFic + ".jpg";
-  nomFic.replaceAll(/'|!|\?|\.|"|:|\$/g, "");
+  var newnomFic = nomFic.replaceAll(/'|!|\?|\.|"|:|\$/g, "");
+  newimg.src = "albumsMini/" + newnomFic + ".jpg";
   divPanel.appendChild(newimg);
 
-  // idbd
   // creation input valeur bd
-
   var inputbd = document.createElement("input");
   inputbd.setAttribute("type", "hidden");
   inputbd.innerHTML = idbd
   divPanel.appendChild(inputbd);
-
-  // var inputbd = document.createElement("value");
-  // inputbd.setAttribute("type", "text");
-  // inputbd.innerHTML= idbd
-  // divPanel.appendChild(inputbd);
 
   // titre
   var titre = document.createElement("titre");
@@ -75,108 +78,104 @@ function createCards(bdd) {
   art.innerHTML = nomart.nom;
   divPanel.appendChild(art);
 
-  // // serie
+  // serie
   var groupe = document.createElement("serie");
-  groupe.setAttribute("class", "card-text2")
+  groupe.setAttribute("class", "card-text2");
   groupe.innerHTML = "serie: " + serie.nom;
   divPanel.appendChild(groupe);
 
   // prix
   var prix = document.createElement("prix");
-  prix.setAttribute("class", "card-text3")
+  prix.setAttribute("class", "card-text3");
   prix.innerHTML = "prix: " + bdd.prix + " €";
   divPanel.appendChild(prix);
 
 
-  function localStock(thing){
-    var detailBd= localStorage.detailBd=idbd;
-    window.location.href="detailBD.html";
-    console.log(detailBd);
-  }
 }
-
-
-initCard();
+// stockages des données
+function localStock(thing) {
+  var detailBd = localStorage.detailBd = idbd;
+  window.location.href = "detailBD.html";
+  console.log(detailBd);
+}
 
 //  gestion mdp/mail
-var pass = document.getElementById("password")
-var mail= document.getElementById("email")
+// function checkform(){
+//   console.log("1"),
+//   checkPassword(pass);
+//  checkMail(mail);
+// checkpass();
+//  checkmail();
+// }
 
-
+// travail
 
 // fonction control entrant de mdp
+// function checkPassword(inputtext) {
 
-function checkPassword(pass) {
-  
-  var decipass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
-  var msg= "Votre Mot de passe doit faire entre 8 et 15 caractères, contenir au moins 1 lettre majuscule, 1 lettre minucule, 1 chiffre et 1 caractère spécial!";
-  if (pass.value.match(decipass)) {
-    alert('Correct, try another...')
-    
-    return true;
-  }
-  else {
-    alert(msg)
-    console.log(checkPassword());
-    return false;
-  }
-}
+//   if (inputtext.value.match(decipass)) {
 
-
-// fonction pour avoir un joli validateur
-// function check(input) {
-//   if (checkPassword(input) == false)
-//   {
-//     input.setCustomValidity( "Votre Mot de passe doit faire entre 8 et 15 caractères, contenir au moins 1 lettre majuscule, 1 lettre minucule, 1 chiffre et 1 caractère spécial!");
-//   } 
-//   else 
-//   {
-//     // input is fine -- reset the error message
-//     input.setCustomValidity('');
+//     alert('Correct, try another...')
+//     return true;
+//   }
+//   else {
+//     console.log("2");
+//     invalidpass="Votre Mot de passe doit faire entre 8 et 15 caractères, contenir au moins 1 lettre majuscule, 1 lettre minucule, 1 chiffre et 1 caractère spécial!"
+//     inputtext.preventDefault(); // bloque la soumission
+//     return false;
 //   }
 // }
-// check();
+// // fonction control entrant de mail
+// function checkMail(inputtext) {
+//   console.log(checkMail);
+
+//   if (inputtext.value.match(decimail)) {
+//     alert('Correct, try another...')
+//     return true;
+//   }
+//   else {
+//     console.log("69");
+//     invalidmail="Veuillez entrer une adresse mail valide."
+//     inputtext.preventDefault(); // bloque la soumission
+//     return false;
+//   }
+// }
 
 
-// fonction control sortant mdp, non submit en cas de fausse erreur.
 
+// travail tout recommencer
 
-// (function() {
-//   'use strict';
-//   window.addEventListener('load', function() {
-//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//     var forms = document.getElementById('invalidPassword');
-//     // Loop over them and prevent submission
-//     var validation = Array.prototype.filter.call(forms, function(form) {
-//       form.addEventListener('submit', function(event) {
-//         if (form.checkPassword(input) === false) {
-//           event.preventDefault();
-//           event.stopPropagation();
-//         }
-//         form.classList.add('was-validated');
-//       }, false);
-//     });
-//   }, false);
-// })();
+function checkpass(pass){
+  
+  if (pass.value.match(decipass)==true) {
+   console.log("2");
+    // var divpass=document.getElementById("passwee");
+    // var ipass=document.createElement("div");
 
-// gestion mail
-// mm fonction que pour password, control entrant
-
-function checkMail(inputtext) {
-  console.log(checkMail);
-  var decimail=/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-  var msg2="Veuillez entrer une adresse mail valide." 
-  if (inputtext.value.match(decimail)) {
-    alert('Correct, try another...')
-    return true;
+    // ipass.setAttribute("class", "invalid-feedback");
+    // ipass.innerhtml="Votre Mot de passe doit faire entre 8 et 15 caractères, contenir au moins 1 lettre majuscule, 1 lettre minucule, 1 chiffre et 1 caractère spécial!"
+    // divpass.appendChild(ipass);
+    
+    
+    // event.preventDefault(); // bloque la soumission
   }
-  else {
-    alert(msg2);
-    return false;
+  else{
+    console.log("3");
   }
+
 }
-// control sortant et non submit en cas erreur.
-// fonction pour check la validité de mes champs par click submit
+// checkmail(){
+//   if (inputtext.value.match(decimail)) {
+//     alert('Correct, try another...');
+//     return true;
+//   }
+//   else {
+//     console.log("69");
+//     inputtext.preventDefault(); // bloque la soumission
+//     return false;
+//   }
+// }
+
 
 
 
